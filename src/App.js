68dcea8dda796ddca7d3986e37547fa7fe366702
Pouya panahzadeh <Pouyapanahzadeh@gmail.com>
2023-07-13 21:20:53 +0200

    add structure and dropzone v1

--- src/App.js
+++ src/App.js
@@ -1,90 +1,131 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Upload from "./components/Upload";
 
 function App() {
-  const logoStyle = {
-    width: '100%',
-    maxWidth: '300px',
-  };
+    const [invoiceData, setInvoiceData] = useState();
+    useEffect(() => {
+        fetchInvoice();
+    }, []);
 
-  return (
-    <table cellPadding="0" cellSpacing="0">
-      <tbody>
-        <tr className="top">
-          <td colSpan="2">
-            <table>
-              <tbody>
-                <tr>
-                  <td className="title">
-                    <img
-                      src="cai_logo.svg"
-                      style={logoStyle}
-                      alt="logo"
-                    />
-                  </td>
+    const fetchInvoice = async () => {
+        const data = await fetch("http://localhost:3003/inovice");
+        const set = await data.json();
+        setInvoiceData(set);
+    };
 
-                  <td>
-                    Invoice #: 39291 <br />
-                    Created: 17/07/2021 <br />
-                    Due: 17/08/2021
-                  </td>
-                </tr>
-              </tbody>
+    const logoStyle = {
+        width: "100%",
+        maxWidth: "300px",
+    };
+
+    if (!invoiceData) {
+        return <>Loading...</>;
+    }
+    return (
+        <>
+            <table cellPadding="0" cellSpacing="0">
+                <tbody>
+                    <tr className="top">
+                        <td colSpan="2">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td className="title">
+                                            <img
+                                                src="cai_logo.svg"
+                                                style={logoStyle}
+                                                alt="logo"
+                                            />
+                                        </td>
+
+                                        <td>
+                                            Invoice #:
+                                            {invoiceData.id
+                                                ? invoiceData.id
+                                                : "Invoice Number"}
+                                            <br />
+                                            Created:{" "}
+                                            {invoiceData.createdAt
+                                                ? invoiceData.createdAt
+                                                : "Date"}
+                                            <br />
+                                            Due:{" "}
+                                            {invoiceData.dueAt
+                                                ? invoiceData.dueAt
+                                                : "Due Date"}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr className="information">
+                        <td colSpan="2">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            {invoiceData.address
+                                                ? invoiceData.address
+                                                : "address line #1"}
+                                            <br />
+                                            {invoiceData.address2
+                                                ? invoiceData.address2
+                                                : "address line #2"}
+                                        </td>
+
+                                        <td>
+                                            {invoiceData.company
+                                                ? invoiceData.company
+                                                : "Company name"}
+                                            <br />
+                                            {invoiceData.fullName
+                                                ? invoiceData.fullName
+                                                : "Client fullName"}
+                                            <br />
+
+                                            {invoiceData.email
+                                                ? invoiceData.email
+                                                : "Company email"}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr className="heading">
+                        <td>Item</td>
+
+                        <td>Price</td>
+                    </tr>
+
+                    <tr className="item last">
+                        <td>Death Star</td>
+                        <td>1100,39 EUR</td>
+                    </tr>
+                    <tr className="item last">
+                        <td>Star destroyer</td>
+                        <td>399,99 EUR</td>
+                    </tr>
+
+                    <tr className="total">
+                        <td></td>
+
+                        <td>Total: 1500,38 EUR</td>
+                    </tr>
+                    <tr className="vat">
+                        <td></td>
+                        <td>VAT (19%): 285,07 EUR</td>
+                    </tr>
+                </tbody>
             </table>
-          </td>
-        </tr>
 
-        <tr className="information">
-          <td colSpan="2">
-            <table>
-              <tbody>
-                <tr>
-                  <td>
-                    collectAI GmbH.
-                    <br />
-                    20457 Hamburg
-                    <br />
-                    Hamburg, Germany
-                  </td>
-
-                  <td>
-                    Acme, GmbH.
-                    <br />
-                    Bob Hans Jens, The Great <br />
-                    youknowit@star-wars-is-real.pew
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-
-        <tr className="heading">
-          <td>Item</td>
-
-          <td>Price</td>
-        </tr>
-
-        <tr className="item last">
-          <td>Death Star</td>
-          <td>1100,39 EUR</td>
-        </tr>
-        <tr className="item last">
-          <td>Star destroyer</td>
-          <td>399,99 EUR</td>
-        </tr>
-
-        <tr className="total">
-          <td></td>
-
-          <td>Total: 1500,38 EUR</td>
-        </tr>
-        <tr className="vat">
-          <td></td>
-          <td>VAT (19%): 285,07 EUR</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+            <Upload />
+        </>
+    );
 }
 
 export default App;
