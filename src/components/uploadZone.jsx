9ad62d3ec9dd476eb20cdf87ac8de7a37dbe2e42
Pouya panahzadeh <Pouyapanahzadeh@gmail.com>
2023-07-19 15:15:47 +0200

    fixed wrong color

--- src/components/uploadZone.jsx
+++ src/components/uploadZone.jsx
@@ -33,7 +33,7 @@ function Upload({ setJsonData }) {
             {isDragActive ? (
                 <button
                     type="button"
-                    className="relative mt-2 block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="relative mt-2 block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800"
                 >
                     <ArrowUpTrayIcon className="mx-auto h-12 w-12 text-zinc-500" />
                     <span className="mt-2 block text-sm font-medium text-zinc-500">
@@ -43,7 +43,7 @@ function Upload({ setJsonData }) {
             ) : (
                 <button
                     type="button"
-                    className="relative mt-2 block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    className="relative mt-2 block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800"
                 >
                     <ArrowUpTrayIcon className="mx-auto h-12 w-12 text-zinc-500" />
                     <span className="mt-2 block text-sm font-medium text-zinc-500">
